5814e588ce0845bdeb0980dd7144479962dc1565
ruplim <ruplimbora@gmail.com>
2019-03-09 07:10:37 -0600

    Presentational and Container Components

--- helloworld/src/components/DishDetailComponent.js
+++ helloworld/src/components/DishDetailComponent.js
@@ -22,14 +22,13 @@ class DishDetail extends Component {
     render() {
         
         return(
-           
+            <div className="container">
                 <div className="row">
                   <div  className="col-12 col-md-5 m-1">
                     {this.renderDish(this.props.dish)}
                   </div>
                 </div>
-            
-
+            </div>
         );
     }
 }
