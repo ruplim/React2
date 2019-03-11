8e2ff7a681db3408a10de92a0de2db453240671a
ruplim <ruplimbora@gmail.com>
2019-03-10 21:44:52 -0500

    Fetch From Server

--- helloworld/src/components/DishDetailComponent.js
+++ helloworld/src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import { CommentForm } from './CommentForm';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderComments({comments, addComment, dishId}) {
     
@@ -33,7 +34,7 @@ function RenderDish({dish}) {
     if (dish != null)
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
